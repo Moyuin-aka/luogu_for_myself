3a895b29c649502f5445be8c44b640837880a45d
Moyuin-aka <ytmfsssu@gmail.com>
2025-02-18 10:06:50 +0800

    program tidy

--- luogu/branch_1/p5705.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    char a,b,c,d;
-   scanf("%c%c%c.%c",&a,&b,&c,&d);
-   printf("%c.%c%c%c",d,c,b,a);
-   return 0;
-
-}
--- luogu/branch_5/p1765.cpp
+++
@@ -1,6 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int count=0;
-int main(){
-    
-}
